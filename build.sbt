5e7ffbb9cbe3b47d953d8abf3f04b5c756d17dc5
Fredrik Ekholdt <freekh@gmail.com>
2013-11-30 18:43:06 +0100

    init

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "scalaex2013-play"
+
+version := "1.0-SNAPSHOT"
+
+libraryDependencies ++= Seq(
+  jdbc,
+  anorm,
+  cache
+)     
+
+play.Project.playScalaSettings
---
+++ project/Emscripten.scala
@@ -0,0 +1,43 @@
+import sbt._
+import sbt.Keys._
+import java.io.File
+
+object EmscriptenCompiler  {
+  case class EmscriptenCompilerException(filename: String, lineNo: Int, colNo: Int, msg: String) extends Exception
+
+  val ErrorLine = """(.*?):(\d+):(\d+): error: (.*?)""".r
+
+
+  val emcc = "/Users/freekh/Downloads/emsdk_portable/emscripten/1.7.8/emcc "
+
+  def compile(file: File, outputDir: File) = {
+    val logger = sys.process.ProcessLogger(
+      (o: String) => None,
+      (e: String) => e match {
+        case ErrorLine(filename, lineNo, colNo, msg) => throw EmscriptenCompilerException(filename, lineNo.toInt, colNo.toInt, msg)
+      })
+      val outputHtmlFile = new File(outputDir, file.getName.replaceAll("""\..*?$""", ".html"))
+      sys.process.Process(emcc + file.getAbsolutePath + " -o " + outputHtmlFile) ! logger
+  }
+
+}
+
+object EmscriptenKeys {
+  val eccFiles = taskKey[PathFinder]("play-ecc-entry-points")
+}
+
+object EmscriptenSettings {
+  import EmscriptenKeys._
+
+  val settings = Seq(
+    eccFiles <<= (sourceDirectory in Compile) map ( _   ** "*.cpp" )
+  )
+}
+
+
+object EmscriptenPlugin extends Plugin {
+  import EmscriptenKeys._
+
+  override lazy val projectSettings: Seq[Setting[_]] = EmscriptenSettings.settings
+
+}
